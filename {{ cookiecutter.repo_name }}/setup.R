b5490c618d1b21e34cd4d6b24c403da9243c9470
Andrew L. Beam <beam.andrew@gmail.com>
2020-01-25 10:25:27 -0500

    Update setup.R

--- {{ cookiecutter.repo_name }}/setup.R
+++ {{ cookiecutter.repo_name }}/setup.R
@@ -1,4 +1,20 @@
-library(tidyverse)
-
 print("Setting up environment")
-require(tidyverse)
+
+args = commandArgs(trailingOnly=TRUE)
+
+if(!require(tidyverse)) {
+  print('Attemping to install package: tidyverse')
+  install.packages('tidyverse')
+}
+
+if(!require(SqlServerJtds)) {
+  print('Attemping to install package: SqlServerJtds')
+}
+
+if(!require(SqlTools)) {
+  print('Attemping to install package: SqlTools')
+}
+
+if(Sys.getenv("AETNA_USERNAME") == "") {
+  print('Adding username to R profile in ~/.Rprofile')
+}
