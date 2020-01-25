67f502f25eb2097d9868c3ed0158e0db9e93b60c
Andrew L. Beam <beam.andrew@gmail.com>
2020-01-25 09:50:06 -0500

    Create create_cohort.R

---
+++ {{ cookiecutter.repo_name }}/src/create_cohort.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library("SqlServerJtds")
+library("SqlTools")
+
+source('src/utils.R')
+
+options(stringsAsFactors=FALSE)
+username = Sys.getenv("AETNA_USERNAME")
+password = Sys.getenv("AETNA_PASSWORD")
+
+config <- read_csv("data/external/config.csv")
